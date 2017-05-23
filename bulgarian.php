48a6193c3a04698ce1882125ae6a27fff5e4aa18
Seyhan Halil <f3n1xx@mail.bg>
2017-05-24 02:55:09 +0300

    First upload

---
+++ bulgarian.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * MyBB 1.8 English Language Pack
+ * Copyright 2010 MyBB Group, All Rights Reserved
+ * 
+ * $Id: english.php 5766 2012-03-28 11:08:55Z Tomm $
+ */
+
+// The friendly name of the language
+$langinfo['name'] = "Bulgarian (български)";
+
+// The author of the language
+$langinfo['author'] = "NEOhidra, AndroidBG.com & EasyThe";
+
+// The language authors website
+$langinfo['website'] = "http://community.mybb.com/user-23021.html";
+
+// Compatible version of MyBB
+$langinfo['version'] = "1811";
+
+// Sets if the translation includes the Admin CP (1 = yes, 0 = no)
+$langinfo['admin'] = 0;
+
+// Sets if the language is RTL (Right to Left) (1 = yes, 0 = no)
+$langinfo['rtl'] = 0;
+
+// Sets the lang in the <html> on all pages
+$langinfo['htmllang'] = "bg";
+
+// Sets the character set, blank uses the default.
+$langinfo['charset'] = "UTF-8";
+?>
